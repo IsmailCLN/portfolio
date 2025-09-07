296ebf5436ebdbddc7538b68b26fba5d79c68570
ismailcln <ismailcelen95@gmail.com>
2025-09-07 15:51:59 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import App from "./App";
 import About from "./pages/About.tsx";
 import Blog from "./pages/Blog.tsx";
 import Contact from "./pages/Contact.tsx";
-import "./index.css"
+import "./index.css";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/about", element: <About /> },
-  { path: "/blog", element: <Blog /> },
-  { path: "/contact", element: <Contact /> },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <App /> },
+    { path: "/about", element: <About /> },
+    { path: "/blog", element: <Blog /> },
+    { path: "/contact", element: <Contact /> },
+  ],
+  {
+    basename: "/portfolio",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
